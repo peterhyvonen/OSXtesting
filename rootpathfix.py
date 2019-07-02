845e9cf43b80fd67a2d47d79ad228b4ebfb56153
peterhyvonen <peterhyvonen@users.noreply.github.com>
2019-07-01 17:39:15 -0700

    Update and rename pathfix.py to rootpathfix.py

--- rootpathfix.py
+++ rootpathfix.py
@@ -1,4 +1,3 @@
-import fileinput
-  with fileinput.FileInput(filename, inplace=True, backup='.bak') as file:
+import fileinput with fileinput.FileInput(filename, inplace=True, backup='.bak') as file:
   for line in file:
     print(line.replace('<root_directory>', '/Users/travis/build/peterhyvonen/OSXtesting'), end='')
