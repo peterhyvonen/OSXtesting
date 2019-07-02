6306cadb33eb518b4919c8dbb3e7dd0ee63599e5
peterhyvonen <peterhyvonen@users.noreply.github.com>
2019-07-01 17:12:14 -0700

    Create pathfix.py

---
+++ pathfix.py
@@ -0,0 +1,4 @@
+import fileinput
+  with fileinput.FileInput(filename, inplace=True, backup='.bak') as file:
+  for line in file:
+    print(line.replace('<root_directory>', '/Users/travis/build/peterhyvonen/OSXtesting'), end='')
