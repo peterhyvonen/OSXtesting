68cfe4f907e2003154976f90b470210c0fc2f1a9
peterhyvonen <peterhyvonen@users.noreply.github.com>
2019-07-02 11:58:56 -0700

    Update rootpathfix.py

--- rootpathfix.py
+++ rootpathfix.py
@@ -2,7 +2,7 @@ import fileinput
 Path2File = '/Users/travis/build/peterhyvonen/OSXtesting/connector-plugin-sdk/tests/datasets/TestV1/postgres/'
 Path2Config = '/Applications/Tableau Desktop 2019.2.app/Contents/MacOS/tabquerytool'
 FileNames = ['load_batters.sql', 'load_calcs.sql', 'load_staples.sql']
-ConfigFile = ''
+ConfigFile = 'config/tdvt/tdvt_override.ini'
 for FileName in FileNames:
   FileName = Path2File+FileName
   with open(FileName) as f:
