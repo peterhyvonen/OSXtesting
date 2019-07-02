9dd29edc836462928945ce5732acacf70a423654
peterhyvonen <peterhyvonen@users.noreply.github.com>
2019-07-02 11:00:11 -0700

    Update rootpathfix.py

--- rootpathfix.py
+++ rootpathfix.py
@@ -1,7 +1,7 @@
 import fileinput 
 Path2File = '/Users/travis/build/peterhyvonen/OSXtesting/connector-plugin-sdk/tests/datasets/TestV1/postgres/'
 FileNames = ['load_batters.sql', 'load_calcs.sql', 'load_staples.sql']
-foreach FileName in FileNames:
+for FileName in FileNames:
   FileName = Path2File+FileName
   with open(FileName) as f:
     newText=f.read().replace('<root_directory>', '/Users/travis/build/peterhyvonen/OSXtesting')
